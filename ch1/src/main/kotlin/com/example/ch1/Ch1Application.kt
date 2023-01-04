1285e970c01135cf37769e6f01ff3ea71dc41b6d
kkyu8925 <kkyu8925@gmail.com>
2023-01-04 18:44:04 +0900

    init

---
+++ ch1/src/main/kotlin/com/example/ch1/Ch1Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch1Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch1Application>(*args)
+}
